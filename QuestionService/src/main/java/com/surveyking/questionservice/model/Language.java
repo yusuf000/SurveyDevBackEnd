03dae8c5d06ee7e2114cf1ddaad467d2ff0d7a3c
sajid <abdullah.osman@tigerit.com>
2023-10-12 17:03:25 +0600

    Changes related to: 1. Added question service

---
+++ QuestionService/src/main/java/com/surveyking/questionservice/model/Language.java
@@ -0,0 +1,22 @@
+package com.surveyking.questionservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "language")
+public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String name;
+    @Column(nullable = false, unique = true, length = 4)
+    private String code;
+}
