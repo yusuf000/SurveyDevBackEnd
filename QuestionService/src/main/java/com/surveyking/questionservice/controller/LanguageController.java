16255706dca398f156996b028cb34552cd600534
sajid <abdullah.osman@tigerit.com>
2023-11-02 15:24:21 +0600

    Changes related to: 1.Extracting authorities for requests in Question Controller

--- QuestionService/src/main/java/com/surveyking/questionservice/controller/LanguageController.java
+++ QuestionService/src/main/java/com/surveyking/questionservice/controller/LanguageController.java
@@ -4,6 +4,7 @@ import com.surveyking.questionservice.model.entity.Language;
 import com.surveyking.questionservice.service.LanguageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ public class LanguageController {
     }
 
     @GetMapping("")
+    @PreAuthorize("hasAuthority('Interviewer Delete')")
     public ResponseEntity<List<Language>> get(){
         return ResponseEntity.ok(languageService.get());
     }
