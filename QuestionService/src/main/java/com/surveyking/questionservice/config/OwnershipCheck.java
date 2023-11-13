ece4ae03b00b1e3a74ab12538466469adcabb77b
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2023-11-13 15:30:33 +0600

    Changes related to: 1.Checking ownership in QuestionController

--- QuestionService/src/main/java/com/surveyking/questionservice/config/OwnershipCheck.java
+++ QuestionService/src/main/java/com/surveyking/questionservice/config/OwnershipCheck.java
@@ -1,22 +1,60 @@
 package com.surveyking.questionservice.config;
 
+import com.surveyking.questionservice.model.QuestionRequest;
 import com.surveyking.questionservice.model.entity.Project;
+import com.surveyking.questionservice.model.entity.Question;
 import com.surveyking.questionservice.repository.ProjectRepository;
+import com.surveyking.questionservice.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
 public class OwnershipCheck {
     private final ProjectRepository projectRepository;
+    private final QuestionRepository questionRepository;
 
     public boolean checkProjectOwner(String sasCode, String userId){
         Optional<Project> project = projectRepository.findProjectBySasCode(sasCode);
         if(project.isEmpty()) return false;
         else{
             return project.get().getOwner().equals(userId);
+        }
+    }
+
+    public boolean checkProjectMembershipFromQuestionRequest(QuestionRequest request, String userId){
+        Optional<Project> project = projectRepository.findProjectBySasCode(request.getProjectSasCode());
+        if(project.isEmpty()) return false;
+        else{
+            return project.get().getMembers().contains(userId);
+        }
+    }
+
+    public boolean checkProjectMembershipFromQuestionRequests(List<QuestionRequest> requests, String userId){
+        for(QuestionRequest request: requests){
+            if(!checkProjectMembershipFromQuestionRequest(request, userId)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkProjectMembershipFromSasCode(String sasCode, String userId) {
+        Optional<Project> project = projectRepository.findProjectBySasCode(sasCode);
+        if(project.isEmpty()) return false;
+        else{
+            return project.get().getMembers().contains(userId);
+        }
+    }
+
+    public boolean checkProjectMembershipFromQuestionId(Long questionId, String userId) {
+        Optional<Question> question = questionRepository.findById(questionId);
+        if(question.isEmpty()) return false;
+        else{
+            return question.get().getProject().getMembers().contains(userId);
         }
     }
 }
--- QuestionService/src/main/java/com/surveyking/questionservice/controller/QuestionController.java
+++ QuestionService/src/main/java/com/surveyking/questionservice/controller/QuestionController.java
@@ -19,40 +19,55 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @PostMapping("/add")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)")
-    public ResponseEntity<Boolean> add(@RequestBody QuestionRequest request) {
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionRequest(#request, #userId)")
+    public ResponseEntity<Boolean> add(
+            @RequestBody QuestionRequest request,
+            @RequestHeader(value = "userId") String userId
+    ) {
         return ResponseEntity.ok(questionService.add(request));
     }
 
     @PostMapping("/add-all")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)")
-    public ResponseEntity<Boolean> add(@RequestBody List<QuestionRequest> requests) {
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionRequests(#requests, #userId)")
+    public ResponseEntity<Boolean> add(
+            @RequestBody List<QuestionRequest> requests,
+            @RequestHeader(value = "userId") String userId
+    ) {
         return ResponseEntity.ok(questionService.add(requests));
     }
 
     @PostMapping("/delete")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_DELETE)")
-    public ResponseEntity<Boolean> delete(@RequestParam("questionId") Long questionId) {
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_DELETE)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionId(#questionId, #userId)")
+    public ResponseEntity<Boolean> delete(
+            @RequestParam("questionId") Long questionId,
+            @RequestHeader(value = "userId") String userId
+    ) {
         return ResponseEntity.ok(questionService.delete(questionId));
     }
 
     @GetMapping("")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)")
-    public ResponseEntity<List<Question>> get(@RequestParam("sasCode") String sasCode) {
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)" + "&& @ownershipCheck.checkProjectMembershipFromSasCode(#sasCode, #userId)")
+    public ResponseEntity<List<Question>> get(
+            @RequestParam("sasCode") String sasCode,
+            @RequestHeader(value = "userId") String userId
+    ) {
         return ResponseEntity.ok(questionService.get(sasCode));
     }
 
     @GetMapping(value = "", params = "questionId")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)")
-    public ResponseEntity<Optional<Question>> get(@RequestParam("questionId") Long questionId) {
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionId(#questionId, #userId)")
+    public ResponseEntity<Optional<Question>> get(
+            @RequestParam("questionId") Long questionId,
+            @RequestHeader(value = "userId") String userId
+    ) {
         return ResponseEntity.ok(questionService.get(questionId));
     }
 
     @GetMapping(value = "/next")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)")
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_INFO)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionId(#questionId, #userId)")
     public ResponseEntity<Question> getNext(
-            @RequestHeader("userId") String userId,
-            @RequestParam("questionId") Long questionId
+            @RequestParam("questionId") Long questionId,
+            @RequestHeader("userId") String userId
     ) {
         return ResponseEntity.ok(questionService.getNext(userId, questionId));
     }
