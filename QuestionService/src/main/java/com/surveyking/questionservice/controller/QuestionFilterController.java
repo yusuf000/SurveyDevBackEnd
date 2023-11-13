e4daae78d561f644a7b02d38ac432cb10decd56e
Abdullah Al Osman Sajid <abdullah.osman@tigerit.com>
2023-11-13 16:05:56 +0600

    Changes related to: 1.Checking ownership in QuestionFilterController

--- QuestionService/src/main/java/com/surveyking/questionservice/controller/QuestionFilterController.java
+++ QuestionService/src/main/java/com/surveyking/questionservice/controller/QuestionFilterController.java
@@ -6,10 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/v1/question-filter")
@@ -17,14 +14,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class QuestionFilterController {
     private final QuestionFilterService questionFilterService;
     @PostMapping("/add")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)")
-    public ResponseEntity<Boolean> add(@RequestBody QuestionFilterRequest request){
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_CREATE)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionId(#request.questionId, #userId)")
+    public ResponseEntity<Boolean> add(
+            @RequestBody QuestionFilterRequest request,
+            @RequestHeader("userId") String userId
+    ){
         return ResponseEntity.ok(questionFilterService.add(request));
     }
 
     @PostMapping("/delete")
-    @PreAuthorize("hasAuthority(@Privilege.QUESTION_DELETE)")
-    public ResponseEntity<Boolean> add(@RequestParam Long questionId){
+    @PreAuthorize("hasAuthority(@Privilege.QUESTION_DELETE)" + "&& @ownershipCheck.checkProjectMembershipFromQuestionId(#questionId, #userId)")
+    public ResponseEntity<Boolean> add(
+            @RequestParam Long questionId,
+            @RequestHeader("userId") String userId
+    ){
         return ResponseEntity.ok(questionFilterService.delete(questionId));
     }
 }
