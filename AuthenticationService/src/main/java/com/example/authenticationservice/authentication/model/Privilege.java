a910594e5700460f3c105d954ff8368dcfcb481b
sajid <abdullah.osman@tigerit.com>
2023-11-29 11:30:56 +0600

    Changes related to: 1.Changed @Data to @Getter @Setter

--- AuthenticationService/src/main/java/com/example/authenticationservice/authentication/model/Privilege.java
+++ AuthenticationService/src/main/java/com/example/authenticationservice/authentication/model/Privilege.java
@@ -1,12 +1,10 @@
 package com.example.authenticationservice.authentication.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Getter
+@Setter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
--- AuthenticationService/src/main/java/com/example/authenticationservice/authentication/model/SecretKey.java
+++ AuthenticationService/src/main/java/com/example/authenticationservice/authentication/model/SecretKey.java
@@ -2,10 +2,12 @@ package com.example.authenticationservice.authentication.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "secret_key")
